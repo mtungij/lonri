793bbbcf4387281b13027b152653153af4879b92
mtungij <jamesmtungi2013@gmail.com>
2024-10-23 02:05:39 +0300

    login

--- resources/views/livewire/pages/auth/login.blade.php
+++ resources/views/livewire/pages/auth/login.blade.php
@@ -38,7 +38,7 @@ new #[Layout('layouts.guest')] class extends Component
                     <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                     <input type="email" wire:model="form.email" name="email" id="email" 
                         class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
-                        placeholder="name@company.com" required />
+                        placeholder="" required />
                     <x-input-error :messages="$errors->get('form.email')" class="mt-2" />
                 </div>
                 <div>
